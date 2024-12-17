c344e5ce62fdd447cbd8dc0237900bf463f551ad
MiguelSarmientoo <miguel.sarmiento@tecsup.edu.pe>
2024-12-17 16:58:44 -0500

    commit

--- config/database.js
+++ config/database.js
@@ -1,25 +1,33 @@
-require('dotenv').config(); 
-
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
+// Configuración de Sequelize
 const sequelize = new Sequelize(
-  process.env.DB_NAME,    
-  process.env.DB_USER,    
-  process.env.DB_PASSWORD, 
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST, 
-    dialect: process.env.DB_DIALECT, 
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
     port: process.env.DB_PORT,
+    logging: console.log, 
   }
 );
 
-// Función para probar conexioon rapida
 const testConnection = async () => {
   try {
     await sequelize.authenticate();
     console.log('Conexión a la base de datos establecida correctamente.');
   } catch (error) {
-    console.error('No se pudo conectar a la base de datos:', error);
+    console.error('Error al conectar a la base de datos:');
+    console.error('Detalles:', {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_DIALECT,
+      port: process.env.DB_PORT,
+    });
+    console.error(error.message);
   }
 };
 
